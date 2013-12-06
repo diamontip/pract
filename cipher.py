97b1a27b5389753ca7190c0b9147ff468b7301e2
Shanmuga Raja <geekraja@gmail.com>
2013-12-06 19:15:20 +0530

    Update cipher.py

--- cipher.py
+++ cipher.py
@@ -1,11 +1,27 @@
 '''
 ToDo:
-1. cipher program
-
+1. cipher by adjusting char positions - completed
+2. cipher by dictionary
+3. decrypt for both cipher 
+4. option to convery text, file
+5. save the new file
+6. delete the original file
 
 '''
 import string
-shift_amt = 13
-alphabet = string.ascii_lowercase
-tab =string.maketrans(alphabet,alphabet[shift_amt:] + alphabet[:shift_amt])
-print "shanmuga raja".translate(tab)
+
+print "\nSelect your encrypt method: \n"
+print "1. one"
+print "2. two"
+selection = raw_input (" > ").lower()
+if selection in ['1', 'one']:
+	shift_amt = 1
+	alphabet = string.ascii_lowercase
+	tab =string.maketrans(alphabet,alphabet[shift_amt:] + alphabet[:shift_amt])
+	print "abcdefghijklmnopqrstuvwxyz".translate(tab)
+elif selection in ['2','two']:
+	print "pimbalikka pilabhi"	
+
+else: 
+	print "you didnt make a valid choice"
+	
