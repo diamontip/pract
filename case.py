842acae410746f1eefa5f2a7ce9fba44315b5c3f
Shanmuga Raja <geekraja@gmail.com>
2014-02-27 20:58:10 +0530

    Create case.py

---
+++ case.py
@@ -0,0 +1,14 @@
+# collecting text input
+text = input('text:')
+
+#converting all text to upper case
+text2 = text.upper()
+
+#printing the converted text
+print (text2)
+
+#converting all text to lower case
+text3 = text.lower()
+
+#printing the converted text
+print (text3)
