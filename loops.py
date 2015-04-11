78e1867d1792e3679f23c68de6e06f71e2905b6f
Shanmuga Raja <geekraja@gmail.com>
2015-04-11 19:24:15 +0530

    Create loops.py

---
+++ loops.py
@@ -0,0 +1,18 @@
+#for loop
+for letter in 'rantsonsoftware':
+	print 'current letter :', letter
+print 'www.rantsonsoftware.com \n'	
+
+#nested loop
+for i in range(0,3): 
+	for i in range(0,5):
+		print 'number is %d' %(i)
+	print 'outter loop'	
+print 'www.rantsonsoftware.com \n'
+
+#while loop
+number = 0 
+while (number <= 9):
+	print 'thenumber is: ', number
+	number = number + 1
+print 'www.rantsonsoftware.com \n'	
