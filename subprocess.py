aaf29cc6ddd06ce237c128c6157b67450c13b223
Shanmuga Raja <geekraja@gmail.com>
2015-04-11 23:21:34 +0530

    added some more functions.

--- subprocess.py
+++ subprocess.py
@@ -1,21 +1,37 @@
+'''
+python script to collect system information using subprocess library. 
+'''
+
 #system information gather script. 
 
 import subprocess
 
+def shell_func():
+	print "Setting the shell argument to a true value"
+	subprocess.call('echo $HOME',shell=True)
+
+
 def uname_func():
 	uname = 'uname'
 	uname_arg='-a'
-	print "collecting system information with %s command:\n" %uname
+	print "gathering system information with %s command:\n" %uname
 	subprocess.call([uname,uname_arg])
 	
 def disk_func():
 	diskspace = "df"
 	diskspace_arg = "-h"
-	print "collecting diskspace information %s command: \n" % diskspace
+	print "gathering diskspace information %s command: \n" % diskspace
 	subprocess.call([diskspace,diskspace_arg])
+
+def in_out_func():
+	output = subprocess.check_output(['ls','-1'])
+	print 'have %d bytes in output' % len(output)
+	print output
 	
 def main():
+	shell_func()
 	uname_func()
 	disk_func()
+	in_out_func()
 
 main()
